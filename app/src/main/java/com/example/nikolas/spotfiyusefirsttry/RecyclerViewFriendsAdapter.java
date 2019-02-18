c2404368999b35a372913d516eb93fd9c1282d76
Andreas Koj <kojandreas@gmail.com>
2019-02-18 17:50:27 +0100

    fix updating friends list in QuizLobbyActivity, implemented profile pictures in firndsList

--- app/src/main/java/com/example/nikolas/spotfiyusefirsttry/RecyclerViewFriendsAdapter.java
+++ app/src/main/java/com/example/nikolas/spotfiyusefirsttry/RecyclerViewFriendsAdapter.java
@@ -5,6 +5,7 @@ import android.graphics.BitmapFactory;
 import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.widget.RecyclerView;
+import android.util.Base64;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,6 +19,8 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
+import de.hdodenhof.circleimageview.CircleImageView;
+
 //import de.hdodenhof.circleimageview.CircleImageView;
 
 public class RecyclerViewFriendsAdapter extends RecyclerView.Adapter<RecyclerViewFriendsAdapter.ViewHolder>{
@@ -26,7 +29,7 @@ public class RecyclerViewFriendsAdapter extends RecyclerView.Adapter<RecyclerVie
     private ArrayList<String> profilePictures;
     private FirebaseDatabase database = FirebaseDatabase.getInstance();
     private Iterator<Map.Entry<String, Friend>> iter;
-
+    private Bitmap bmp;
 
     Bitmap profileBmp;
     View.OnClickListener onClickListener;
@@ -37,6 +40,8 @@ public class RecyclerViewFriendsAdapter extends RecyclerView.Adapter<RecyclerVie
         this.onClickListener = onClickListener;
         profilePictures = new ArrayList<>();
         this.friends = UserManager.getInstance().getUserInfo().getFriends();
+
+        //set the iterator to read keys of the hashmap
         iter = friends.entrySet().iterator();
     }
 
@@ -52,9 +57,18 @@ public class RecyclerViewFriendsAdapter extends RecyclerView.Adapter<RecyclerVie
     public void onBindViewHolder(@NonNull ViewHolder viewHolder, final int i) {
 
         if(iter.hasNext()){
-            Map.Entry<String, Friend> pair = (Map.Entry<String, Friend>) iter.next();
-            Log.d(TAG, "onBindViewHolder:  " + pair.getKey() + " = " + pair.getValue() );
+            Map.Entry<String, Friend> pair = iter.next();
             viewHolder.userName.setText(pair.getKey());
+
+
+            //Log.d(TAG, "profile: "+ friends.get(pair.getKey()).getProfilePicture());
+            byte[] byteArray = Base64.decode(friends.get(pair.getKey()).getProfilePicture(), 0);
+            Log.d(TAG, "onSuccess: " + byteArray);
+
+            bmp = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
+            Log.d(TAG, "onSuccess: " + bmp);
+
+            viewHolder.profilePicture.setImageBitmap(bmp);
         }
 
         viewHolder.elementLayout.setOnClickListener(onClickListener);
@@ -67,14 +81,14 @@ public class RecyclerViewFriendsAdapter extends RecyclerView.Adapter<RecyclerVie
 
     public class  ViewHolder extends RecyclerView.ViewHolder {
 
-        //CircleImageView profilePicture;
+        CircleImageView profilePicture;
         TextView userName;
         ConstraintLayout elementLayout;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             userName = itemView.findViewById(R.id.recyclerFriends_userName);
-            //profilePicture = itemView.findViewById(R.id.recyclerFriends_profilePicture);
+            profilePicture = itemView.findViewById(R.id.recyclerFriends_profilePicture);
             elementLayout = itemView.findViewById(R.id.recyclerFriends_layout);
         }
     }
