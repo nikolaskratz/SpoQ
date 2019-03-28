9b58c91e2333612db27781f0256e4d3cf374328c
Andreas Koj <kojandreas@gmail.com>
2019-03-28 21:41:36 +0100

    test commit

--- app/src/main/java/com/example/nikolas/spotfiyusefirsttry/SpotifyWebApiUtils.java
+++ app/src/main/java/com/example/nikolas/spotfiyusefirsttry/SpotifyWebApiUtils.java
@@ -13,7 +13,7 @@ import java.nio.charset.Charset;
 
 import retrofit.client.UrlConnectionClient;
 
-
+//test
 //GET https://accounts.spotify.com/authorize?client_id=5fe01282e44241328a84e7c5cc169165&response_type=code&redirect_uri=https%3A%2F%2Fexample.com%2Fcallback&scope=user-read-private%20user-read-email&state=34fFs29kd09
 
 public class SpotifyWebApiUtils {
